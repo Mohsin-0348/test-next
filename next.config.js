cc203b111374b85fd11f63cc0e6eb11a71c32c78
jim <mohsin.mahbub.jim@gmail.com>
2022-02-24 11:07:03 +0600

    next.config.js file updated

--- next.config.js
+++ next.config.js
@@ -4,3 +4,21 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withImages = require('next-images')
+module.exports = withImages()
+
+module.exports = withImages({
+images: {
+    loader: "imgix",
+    path: "",
+},
+exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+})
